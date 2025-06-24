fa649b5c9abfb0a5520225439e32b89bfbd8e37c
aki ruthwik39 <aki.ruthwik39@gmail.com>
2025-06-24 01:09:36 +0000

    still same issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,7 +26,17 @@ export async function middleware(request: NextRequest) {
           return request.cookies.get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
-          // If the cookie is set, update the response cookies.
+          // If the cookie is set, update the request and response cookies.
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
           response.cookies.set({
             name,
             value,
@@ -34,7 +44,17 @@ export async function middleware(request: NextRequest) {
           })
         },
         remove(name: string, options: CookieOptions) {
-          // If the cookie is removed, update the response cookies.
+          // If the cookie is removed, update the request and response cookies.
+          request.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
           response.cookies.set({
             name,
             value: '',
