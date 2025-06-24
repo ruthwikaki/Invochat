9067a360c9e80bc7e05d66883266573a833d6ad3
aki ruthwik39 <aki.ruthwik39@gmail.com>
2025-06-24 04:08:15 +0000

    `headers` field cannot be empty for route {"source":"/:path*","headers":

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
