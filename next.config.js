6b0c74e18f7859909af898a2fbd4af2f09c32e74
aki ruthwik39 <aki.ruthwik39@gmail.com>
2025-06-25 19:14:17 +0000

    Error: EINVAL: invalid argument, readlink 'C:\Users\akiru\OneDrive\Deskt

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   webpack: (config, { dev }) => {
     if (dev) {
       config.cache = false;
+      config.watchOptions = {
+        poll: 1000, // Check for changes every second
+        aggregateTimeout: 300, // Delay before rebuilding
+      };
     }
     return config;
   },
