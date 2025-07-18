b53f6a3e58ef88eaf20ed036eaba13467cbd561a
aki ruthwik39 <aki.ruthwik39@gmail.com>
2025-07-18 23:44:18 +0000

    if you dont get what is this issue I will provide the more details

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -76,28 +77,24 @@ const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
     return null
   }
 
-  return (
-    <style
-      dangerouslySetInnerHTML={{
-        __html: Object.entries(THEMES)
-          .map(
-            ([theme, prefix]) => `
+  const cssText = Object.entries(THEMES)
+    .map(
+      ([theme, prefix]) => `
 ${prefix} [data-chart=${id}] {
 ${colorConfig
-  .map(([key, itemConfig]) => {
-    const color =
-      itemConfig.theme?.[theme as keyof typeof itemConfig.theme] ||
-      itemConfig.color
-    return color ? `  --color-${key}: ${color};` : null
-  })
-  .join("\n")}
+.map(([key, itemConfig]) => {
+  const color =
+    itemConfig.theme?.[theme as keyof typeof itemConfig.theme] ||
+    itemConfig.color
+  return color ? `  --color-${key}: ${color};` : null
+})
+.join("\n")}
 }
 `
-          )
-          .join("\n"),
-      }}
-    />
-  )
+    )
+    .join("\n");
+
+  return <style>{cssText}</style>
 }
 
 const ChartTooltip = RechartsPrimitive.Tooltip
