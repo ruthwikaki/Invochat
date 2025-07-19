04ef248bd6e2e8fce405ac4a1f679ddae0376c36
aki ruthwik39 <aki.ruthwik39@gmail.com>
2025-07-19 21:40:24 +0000

    ✓ Compiled successfully    Linting and checking validity of types  ...Fa

--- src/app/(app)/suppliers/page.tsx
+++ src/app/(app)/suppliers/page.tsx
@@ -1,7 +1,7 @@
 
 import { getSuppliersData } from '@/app/data-actions';
 import { SuppliersClientPage } from './suppliers-client-page';
-import { AppPage, AppPageHeader } from '@/components/ui/page';
+import { AppPageHeader } from '@/components/ui/page';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
