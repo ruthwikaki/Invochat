699130f6c77b57aa49dc82104bf59af61fb7e12d
aki ruthwik39 <aki.ruthwik39@gmail.com>
2025-07-20 15:45:11 +0000

    ✓ Compiled successfully    Linting and checking validity of types  .Fail

--- app/src/app/import/actions.ts
+++ app/src/app/import/actions.ts
@@ -291,4 +291,5 @@ export async function handleDataImport(formData: FormData): Promise<ImportResult
         }
         return { success: false, isDryRun, summaryMessage: `An unexpected server error occurred: ${errorMessage}` };
     }
-}
+
+    
