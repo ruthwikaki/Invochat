b0311ee7949f224a136ccf6936c00b7161f6d327
aki ruthwik39 <aki.ruthwik39@gmail.com>
2025-07-25 20:20:51 +0000

    oh please dont wate my time just fix it corectly

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -4,8 +4,9 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/features/**/*.{js,ts,jsx,tsx,mdx}',
 	],
   prefix: "",
