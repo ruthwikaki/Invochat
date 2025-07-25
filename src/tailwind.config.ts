b673d45c37fcf5f8fe2bef54c790b9fe625ba550
aki ruthwik39 <aki.ruthwik39@gmail.com>
2025-07-25 23:34:13 +0000

    the csrf issue was resolved but the landing page issues still there why

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -4,11 +4,11 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   prefix: "",
   theme: {
     container: {
