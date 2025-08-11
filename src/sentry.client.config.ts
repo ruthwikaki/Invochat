b2610ba5f6fd1096ab666b913663b7aefbeb82a8
aki ruthwik39 <aki.ruthwik39@gmail.com>
2025-08-11 00:23:14 +0000

    Great progress. Those 5 failures boil down to 3 things: missing/strict a

--- src/sentry.client.config.ts
+++ src/sentry.client.config.ts
@@ -2,15 +2,18 @@
 // This file configures the Sentry browser client for error reporting.
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+const isTest = process.env.NODE_ENV === 'test' || !!process.env.PLAYWRIGHT_TEST;
 
-  // You can remove this option if you're not planning to use the Sentry Replay feature.
-  replaysOnErrorSampleRate: 1.0,
+if (!isTest) {
+  Sentry.init({
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample it in production
-  replaysSessionSampleRate: 0.1,
-});
+    // You can remove this option if you're not planning to use the Sentry Replay feature.
+    replaysOnErrorSampleRate: 1.0,
 
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample it in production
+    replaysSessionSampleRate: 0.1,
+  });
+}
